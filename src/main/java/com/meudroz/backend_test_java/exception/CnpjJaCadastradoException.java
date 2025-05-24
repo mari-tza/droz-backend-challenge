88f965cbd8087910416a9978ff65e65b8cb98283
mari <maaritzacastro@gmail.com>
2025-05-24 20:37:10 -0300

    chore: finaliza ajustes finais com validacoes, liquidbase, testes, criacao de application-test e identacao

---
+++ src/main/java/com/meudroz/backend_test_java/exception/CnpjJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.meudroz.backend_test_java.exception;
+
+public class CnpjJaCadastradoException extends RuntimeException {
+  public CnpjJaCadastradoException(String message) {
+    super(message);
+  }
+}
